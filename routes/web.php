c5c93eecaa3185fe782ee367023921eccbd090e0
“Yomar” <team.yob10@gmail.com>
2023-05-21 06:47:48 -0400

    Installed inertia succesfully

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return Inertia::render('Welcome', [
+        'user' => 'David Guilarte'
+    ]);
 });
